61e229fbd617da381ea8b1c049da488dc6247648
Josh Weisman <josh@joshweismanfilm.com>
2017-07-15 16:07:13 -0700

    Add Bootstrap, and navigation bar at the top of the site

---
+++ app/controllers/pages_controller.rb
@@ -0,0 +1,21 @@
+class PagesController < ApplicationController
+
+  def home
+  end
+
+  def movie_outlines
+  end
+
+  def tv_outlines
+  end
+
+  def create_outline
+  end
+
+  def glossary
+  end
+
+  def login_signup
+  end
+
+end
--- config/routes.rb
+++ config/routes.rb
@@ -1,3 +1,10 @@
 Rails.application.routes.draw do
+  root "pages#home"
+  get "movie_outlines" => "pages#movie_outlines" #Creates movie_outlines_path
+  get "tv_outlines" => "pages#tv_outlines" #Creates tv_outlines_path
+  get "create_outline" => "pages#create_outline" #Creates create_outline_path
+  get "glossary" => "pages#glossary" #Creates glossary_path
+  get "login_signup" => "pages#login_signup" #Creates login_signup_path
+
   # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
 end
